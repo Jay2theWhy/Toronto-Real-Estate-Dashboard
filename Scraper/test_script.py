9b85178051c6b6aa8f60587f3829718c3e9f0ab7
Jing Yang Fan <jyfan16@hotmail.com>
2022-03-14 00:27:53 -0400

    Used Selenium webdriver to get around anti-webscraping in Zolo.ca

--- Scraper/test_script.py
+++ Scraper/test_script.py
@@ -1,6 +1,23 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 
-URL = "https://www.zolo.ca"
-page = requests.get(URL)
+# driver = webdriver.Chrome('/Users/jingyangfan/.wdm/drivers/chromedriver/mac64_m1/99.0.4844.51/chromedriver')
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://www.zolo.ca')
 
-print(page.text)
+print(driver.page_source)
+
+
+# # get html data using requests
+# URL = "https://www.zolo.ca"
+
+# page = requests.get(URL)
+
+# # parse html data using BeautifulSoup
+# soup = BeautifulSoup(page.content, "html.parser")
+
+# # identify id
+# results = soup.find(id='listing_container')
+# print(page.text)
