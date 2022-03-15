e19eab44efb10c4ac9867768ec78f6425fbef617
Jing Yang Fan <jyfan16@hotmail.com>
2022-03-14 22:54:42 -0400

    Changed filename. Got all listing links from html

---
+++ Scraper/zolo_scraper.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# driver = webdriver.Chrome('/Users/jingyangfan/.wdm/drivers/chromedriver/mac64_m1/99.0.4844.51/chromedriver')
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+def get_page(page_num):
+    url = f'https://www.zolo.ca/toronto-real-estate/page-{page_num}'
+    driver.get(url)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
+    return soup
+
+# get links for all housings
+soup = get_page(1)
+html = soup.find('main', id = "listing_container")\
+    .find('section', id = 'gallery')\
+    .find('div', class_ = 'listings-wrapper xs-flex xs-flex-wrap xs-grid sm-grid-cols-2 lg-grid-cols-3 xl-grid-cols-4 xs-gap-2 sm-gap-3 xs-mx2- sm-mx0')\
+    .findAll('article', class_ = 'card-listing xs-relative xs-full-height xs-flex xs-flex-column-reverse xs-flex-justify-end rounded shadow-card xs-overflow-hidden new')
+
+for div in html:
+    div = div.find('div', class_ = 'card-listing--image fill-white xs-relative xs-overflow-hidden')
+    print(div.find('a')['href'])
+
+# # get html data using requests
+# URL = "https://www.zolo.ca"
+
+# page = requests.get(URL)
+
+# # parse html data using BeautifulSoup
+# soup = BeautifulSoup(page.content, "html.parser")
+
+# # identify id
+# results = soup.find(id='listing_container')
+# print(page.text)
