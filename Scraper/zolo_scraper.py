3298755d6eaea03c1d40eb1483572114db84ea3e
Jing Yang Fan <jyfan16@hotmail.com>
2022-03-15 20:05:45 -0400

    encapsulated code into functions

--- Scraper/zolo_scraper.py
+++ Scraper/zolo_scraper.py
@@ -14,16 +14,23 @@ def get_page(page_num):
     soup = BeautifulSoup(page_source, 'lxml')
     return soup
 
-# get links for all housings
-soup = get_page(1)
-html = soup.find('main', id = "listing_container")\
-    .find('section', id = 'gallery')\
-    .find('div', class_ = 'listings-wrapper xs-flex xs-flex-wrap xs-grid sm-grid-cols-2 lg-grid-cols-3 xl-grid-cols-4 xs-gap-2 sm-gap-3 xs-mx2- sm-mx0')\
-    .findAll('article', class_ = 'card-listing xs-relative xs-full-height xs-flex xs-flex-column-reverse xs-flex-justify-end rounded shadow-card xs-overflow-hidden new')
+# get links for listing on zolo page
+def get_links(page_link):
+    soup = get_page(page_link)
 
-for div in html:
-    div = div.find('div', class_ = 'card-listing--image fill-white xs-relative xs-overflow-hidden')
-    print(div.find('a')['href'])
+    # parse html
+    html = soup.find('main', id = "listing_container")\
+        .find('section', id = 'gallery')\
+        .find('div', class_ = 'listings-wrapper xs-flex xs-flex-wrap xs-grid sm-grid-cols-2 lg-grid-cols-3 xl-grid-cols-4 xs-gap-2 sm-gap-3 xs-mx2- sm-mx0')\
+        .findAll('article', class_ = 'card-listing xs-relative xs-full-height xs-flex xs-flex-column-reverse xs-flex-justify-end rounded shadow-card xs-overflow-hidden new')
+    
+    # get link for each listing on the page
+    for div in html:
+        div = div.find('div', class_ = 'card-listing--image fill-white xs-relative xs-overflow-hidden')
+        print(div.find('a')['href'])
+
+def get_info(page_link):
+    soup = get_page(page_link)
 
 # # get html data using requests
 # URL = "https://www.zolo.ca"
